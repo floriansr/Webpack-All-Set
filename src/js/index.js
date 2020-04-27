2b6a971b2a0191d2d966afa40378bbe327326f11
floriansr <sueurfloriansf@gmail.com>
2020-04-27 17:01:42 +0200

    bootstrap added via npm, webpack dev server all set, sass

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+import 'bootstrap';
+
+import "../sass/styles.scss";
+import moment from "moment";
+
+console.log("salut")
+
+console.log(moment().format("MMMM Do YYYY"));
+console.log(moment("20111031", "YYYYMMDD").fromNow()); // 8 years ago
+console.log(moment().subtract(10, "days").calendar()); // 05/27/2019
+
+console.log("TEST", process.env.DB_HOST);
+
+const alert = document.getElementById('alert');
+
+alert.innerHTML += `
+
+<div class="alert alert-primary" role="alert">
+  This is a primary alert—check it out!
+</div>
+`;
